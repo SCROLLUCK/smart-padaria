e9a8cb1cb70b32995aa89d67fd932ecfbc8ee4c0
rafaelrdacosta <rafaelrdacosta@gmail.com>
2025-10-10 17:28:57 -0400

    Atualização

--- smart_padaria/smart_padaria.ino
+++ smart_padaria/smart_padaria.ino
@@ -9,7 +9,6 @@
   - Sensor DHT11 (ou DHT22)
   - Display LCD 16x2 (via interface paralela)
   - Protoboard
-  - Potênciometro
   - Resistor de 10K
   - Jumpers para ligação no protoboard
  */
@@ -18,7 +17,7 @@
 #include<DHT.h>                   // Biblioteca DHT para leitura de dados do sensor
 #include<DHT_U.h>                 // Biblioteca Unified Sensor para acesso a eventos e metadados
 #include<LiquidCrystal.h>         // Biblioteca para controle do Display LCD padrão
-#include<IRremote.h>              // Biblioteca para o receptor infravermelho
+
 
 // Objeto LCD: Inicializa a biblioteca com os pinos de interface
 // Pinos: (RS, E, D4, D5, D6, D7) conectados ao Arduino
@@ -28,20 +27,12 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 #define DHTTYPE DHT11              // Define o tipo de sensor que está sendo utilizado (DHT11)
 #define DHTPIN 2                   // Define o pino digital do Arduino ao qual o pino de dados do sensor está conectado  
 
-int RECV_PIN = 3;               // Define o pino digital do Arduino o qual irá receber o sinal do sensor 
-
 // Cria uma instância unificada do sensor DHT, usando o pino e o tipo definidos
 DHT_Unified dht(DHTPIN, DHTTYPE);  
 
 // Variáveis de controle de tempo
-uint32_t delayExibicaoMS = 3000;   // Tempo de exibição de cada tela (2 segundos)
+uint32_t delayExibicaoMS = 2000;   // Tempo de exibição de cada tela (2 segundos)
 unsigned long tempoInicio;         // Armazena o valor de millis() (tempo total em ms desde o boot) no setup()
-unsigned long ultimoTempoExibicao = 0; // Variável para o temporizador não-bloqueante do LCD
-
-//Declararação de um objeto da classe receive
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-
 
 // Variável de controle para alternar as telas
 bool mostrarTempo = false;
@@ -51,11 +42,11 @@ void setup() {
   // Inicializa a comunicação serial a 9600 bps para depuração e monitoramento no PC
   Serial.begin(9600);
 
-  //Inicializa o receptor infravermelho
-  irrecv.enableIRIn();
-
   // Inicializa o sensor DHT
   dht.begin();
+
+   // Variável local que armazena informações sobre o sensor
+  sensor_t sensor;
 
   // Inicializa o Display LCD
   lcd.begin(16, 2);
@@ -79,107 +70,88 @@ void setup() {
 }
 
 void loop() {
-  //Zera os registradores
-  results.value = 0;
-  if (irrecv.decode(&results)) {
-    Serial.print("IR Recebido: ");
-    Serial.println(results.value, HEX); // Imprime o valor em HEX, que é o padrão para comandos IR
-    
-    
-    // EX: Aqui você pode adicionar lógica para o controle remoto (ex: if(resultado.value == 0xFF...) alterar tempoInicio)
-    
-    irrecv.resume(); // Continua à espera do próximo sinal
-  }else{
-    Serial.print("Erro na leitura do infravermelho.");
-  }
+  // Lógica de alternância de tela: O display muda a cada 'delayExibicaoMS'
+  // O valor de 'mostrarTempo' inverte a cada ciclo de exibição
+  mostrarTempo = !mostrarTempo; 
 
-  // Lógica de Alternância de Tela Não-Bloqueante
-  if (millis() - ultimoTempoExibicao >= delayExibicaoMS) {
-    // Inverte a flag e atualiza o tempo da última atualização
-    mostrarTempo = !mostrarTempo;
-    ultimoTempoExibicao = millis(); 
+  if(!mostrarTempo){
+      // Objeto para armazenar o "evento" (leitura) dos sensores.
+      sensors_event_t event;
 
-      if(!mostrarTempo){
-          // Objeto para armazenar o "evento" (leitura) dos sensores.
-          sensors_event_t event;
-
-          // --- Leitura da Temperatura ---
-          dht.temperature().getEvent(&event);
-
-          
-          
-          //Exibição da Temperatura e umidade no Serial Monitor
-          //Serial.println("--------------------");
-
-          if(isnan(event.temperature)){
-            Serial.println("Erro na leitura da Temperatura!");
-          } else{
-            /*Serial.print("Temperatura: ");
-            Serial.print(event.temperature, 1);
-            Serial.println(" ºC");*/
-
-            // 1. Exibição no LCD (Linha 0)
-            lcd.setCursor(0, 0);
-            // Limpar apenas o necessário para não piscar
-            lcd.print("Temp: ");
-            // Imprime o valor com uma casa decimal (1)
-            lcd.print(event.temperature, 1); 
-            lcd.print(" C ");
-          }
+      // --- Leitura da Temperatura ---
+      dht.temperature().getEvent(&event);
       
+      //Exibição da Temperatura e umidade no Serial Monitor
+      Serial.println("--------------------");
 
-          // --- Leitura da Umidade ---
-          // Obtém o evento de umidade e armazena os dados no mesmo objeto 'event'
-          dht.humidity().getEvent(&event); 
+      if(isnan(event.temperature)){
+        Serial.println("Erro na leitura da Temperatura!");
+      } else{
+        Serial.print("Temperatura: ");
+        Serial.print(event.temperature, 1);
+        Serial.println(" ºC");
 
-          if(isnan(event.relative_humidity)){
-            Serial.println("Erro na leitura da Umidade!");
-          }else{
-            /*Serial.print("Umidade: ");
-            Serial.print(event.relative_humidity, 1);
-            Serial.println("%");*/
-
-            // 2. Exibição no LCD (Linha 1)
-            lcd.setCursor(0, 1);
-            lcd.print("Umidade: ");
-            // Imprime o valor com uma casa decimal (1)
-            lcd.print(event.relative_humidity, 1);
-            lcd.print("%  "); 
-          }
-      }else {
-
-        // Calcula o tempo decorrido total em milissegundos
-        unsigned long tempoDecorrido = millis() - tempoInicio; 
-        
-        // Converte milissegundos em minutos e segundos
-        int segundosTotais = tempoDecorrido / 1000;
-        int minutos = segundosTotais / 60;
-        int segundos = segundosTotais % 60; // Resto da divisão é o segundo
-        
-        // Exibe no Serial
-        /*Serial.print("Tempo Decorrido: ");
-        Serial.print(minutos);
-        Serial.print("m ");
-        Serial.print(segundos);
-        Serial.println("s");*/
-
-        // Exibe no LCD
-        lcd.clear(); // Limpa a tela para a nova mensagem
+        // 1. Exibição no LCD (Linha 0)
         lcd.setCursor(0, 0);
-        lcd.print("Fermentacao:");
-        
-        lcd.setCursor(0, 1);
-        // Adiciona um '0' à esquerda se o minuto/segundo for menor que 10 (formato MM:SS)
-        if (minutos < 10) lcd.print("0");
-        lcd.print(minutos);
-        lcd.print("m ");
-        if (segundos < 10) lcd.print("0");
-        lcd.print(segundos);
-        lcd.print("s");
+        // Limpar apenas o necessário para não piscar
+        lcd.print("Temp: ");
+        // Imprime o valor com uma casa decimal (1)
+        lcd.print(event.temperature, 1); 
+        lcd.print(" C ");
       }
+  
+
+      // --- Leitura da Umidade ---
+      // Obtém o evento de umidade e armazena os dados no mesmo objeto 'event'
+      dht.humidity().getEvent(&event); 
+
+      if(isnan(event.relative_humidity)){
+        Serial.print("Erro na leitura da Umidade!");
+      }else{
+        Serial.print("Umidade: ");
+        Serial.print(event.relative_humidity);
+        Serial.println("%");
+
+        // 2. Exibição no LCD (Linha 1)
+        lcd.setCursor(0, 1);
+        lcd.print("Umidade: ");
+        // Imprime o valor com uma casa decimal (1)
+        lcd.print(event.relative_humidity, 1);
+        lcd.print("%  "); 
+      }
+  }else {
+
+    // Calcula o tempo decorrido total em milissegundos
+    unsigned long tempoDecorrido = millis() - tempoInicio; 
+    
+    // Converte milissegundos em minutos e segundos
+    int segundosTotais = tempoDecorrido / 1000;
+    int minutos = segundosTotais / 60;
+    int segundos = segundosTotais % 60; // Resto da divisão é o segundo
+    
+    // Exibe no Serial
+    Serial.print("Tempo Decorrido: ");
+    Serial.print(minutos);
+    Serial.print("m ");
+    Serial.print(segundos);
+    Serial.println("s");
+
+    // Exibe no LCD
+    lcd.clear(); // Limpa a tela para a nova mensagem
+    lcd.setCursor(0, 0);
+    lcd.print("Fermentacao:");
+    
+    lcd.setCursor(0, 1);
+    // Adiciona um '0' à esquerda se o minuto/segundo for menor que 10 (formato MM:SS)
+    if (minutos < 10) lcd.print("0");
+    lcd.print(minutos);
+    lcd.print("m ");
+    if (segundos < 10) lcd.print("0");
+    lcd.print(segundos);
+    lcd.print("s");
   }
 
-  /* Pausa antes da próxima alternância de tela/leitura.
+  // Pausa antes da próxima alternância de tela/leitura.
   // A frequência de atualização de TELA 1 e TELA 2 é controlada por esta linha.
-  delay(delayExibicaoMS);*/
+  delay(delayExibicaoMS);
 }
